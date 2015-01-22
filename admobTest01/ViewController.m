b40546ef33f616c9d6bc570ec09fcb20cf52ce13
tikomo <tikomo@Komorita-no-MacBook-Air.local>
2015-01-22 17:52:21 +0900

    Initial Commit

---
+++ admobTest01/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  admobTest01
+//
+//  Created by tikomo on 2015/01/22.
+//  Copyright (c) 2015年 tikomo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
